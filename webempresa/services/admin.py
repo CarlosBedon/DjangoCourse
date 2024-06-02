37e1a5e5a8f89d3d08d29b55f3f8a6806058ae69
CARLOS BEDON <carlosbedon@outlook.com>
2024-06-02 11:36:56 -0500

    clases

---
+++ webempresa/services/admin.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-final-newline
+from django.contrib import admin
+from .models import Service
+# Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+
+
+admin.site.register(Service, ServiceAdmin)
